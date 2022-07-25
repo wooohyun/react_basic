e485ed3026a282ffafe7e813ceebe30c3187d3eb
wooohyun <woodoll543@gmail.com>
2022-07-25 12:22:12 +0900

    20220725

---
+++ react00/src/Nav.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+    return (
+        <>
+            <ul className="big">
+                <li>menu01</li>
+                <li>menu02</li>
+                <li>menu03</li>
+            </ul>
+        </>
+    )
+}
+
+export default Nav;
